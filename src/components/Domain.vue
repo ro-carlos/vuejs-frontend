07a22495a642447312630efea314bf64d1d6b410
Carlos Andrés Rodríguez Torres <andresrodriguez@worldofficeonline.com>
2020-07-13 18:31:13 -0500

    Display values

--- src/components/Domain.vue
+++ src/components/Domain.vue
@@ -2,7 +2,7 @@
   <div class="col-sm-12 col-md-12">
     <div class="input-group mb-3">
       <input type="text" class="form-control" placeholder="Search by Domain"
-        v-model="domain" @keyup.enter="saveDomain"/>
+        v-model="hostname" @keyup.enter="saveDomain"/>
       <div class="input-group-append">
         <button class="btn btn-outline-success" type="button"
           @click="saveDomain" :disabled='loading'
@@ -12,10 +12,40 @@
         </button>
       </div>
     </div>
-    <b-modal id="domainInformation" ref="domainInformation" title="BootstrapVue">
+
+    <div>
+      <b-card v-if="domain.address" :title="domain.address" class="text-center" bg-variant="sucess">
+        <b-card-text>
+          <div v-if="domain.title"> <strong>Title: </strong> {{domain.title}} <br> </div>
+          <div v-if="domain.logo"> <strong>Logo: </strong> {{domain.logo}} <br> </div>
+          <div v-if="domain.previous_ssl_grade"> <strong>Previous SSL Grade: </strong> {{domain.previous_ssl_grade}} <br> </div>
+          <div v-if="domain.ssl_grade"> <strong>SSL Grade: </strong> {{domain.ssl_grade}} <br> </div>
+          <strong>Servers Changed: </strong> {{domain.servers_changed}} <br>
+          <strong>Is Down: </strong> {{domain.is_down}}
+
+        </b-card-text>
+ 
+      </b-card>
+
+      <br><br>
+      <div class="mt-3">
+        <b-card-group deck>
+          <b-card bg-variant="info" text-variant="white" :header="server.address" class="text-center" v-for="(server, index) in domain.servers" :key="index">
+            <b-card-text>
+              <div v-if="server.ssl_grade"> <strong>SSL Grade: </strong> {{server.ssl_grade}} <br> </div>
+              <div v-if="server.country"> <strong>Country: </strong> {{server.country}} <br> </div>
+              <div v-if="server.owner"> <strong>Owner: </strong> {{server.owner}} <br> </div>
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </div>
+
+    </div>
+
+    <!-- <b-modal id="domainInformation" ref="domainInformation" title="BootstrapVue">
       <p class="my-4">Here is the domain information</p>
-    </b-modal>
-  </div>
+    </b-modal> -->
+  </div> 
 
 
 </template>
@@ -30,36 +60,39 @@ export default {
   name: "domain",
   data() {
     return {
-      domain: "",
-      loading:false
+      hostname: "",
+      loading:false,
+      domain: {}
     };
   },
   methods: {
 
 
     saveDomain() {
-      if(this.domain && this.domain.length > 0){
+      if(this.hostname && this.hostname.length > 0){
         this.loading = true;
         bus.$emit('loading', this.loading);
 
-        DomainService.getDomain(this.domain)
+        DomainService.getDomain(this.hostname)
           .then(response => {
             console.log(response.data);
 
             this.loading = false;
-            this.domain = ""
+            this.hostname = ""
             bus.$emit('loading', this.loading);
 
-            // Add values to modal
             if(response.data.valid){
-              this.$refs['domainInformation'].show()
+              // this.$refs['domainInformation'].show()
+              this.domain = response.data.content;
+              console.log(this.domain)
             }else{
               ToastService.displayErrorMessage(this.$bvToast, "Message", "Invalid Domain");
+              this.domain = null;
             }
-
           })
           .catch(e => {  
             console.log(e);
+            this.domain = null;
             this.loading = false;
             bus.$emit('loading', this.loading);
             ToastService.displayErrorMessage(this.$bvToast, "Message", "Error Browsing Domain");
@@ -69,11 +102,3 @@ export default {
   }
 };
 </script>
-
-<style>
-.submit-form {
-  height: 100%;
-  width: 100%;
-  margin: auto;
-}
-</style>
